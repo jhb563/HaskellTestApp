cebc68947f292a4dc6a144b31689e04aa6cc565b
James Bowen <jhbowen047@gmail.com>
2018-04-24 08:39:55 -0700

    Change env name

--- .circleci/setup_heroku.sh
+++ .circleci/setup_heroku.sh
@@ -3,8 +3,8 @@
 cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
-  password $HEROKU_API_KEY
+  password $HEROKU_PASSWORD
 machine git.heroku.com
   login $HEROKU_LOGIN
-  password $HEROKU_API_KEY
+  password $HEROKU_PASSWORD
 EOF
